fc6297d27899f283ab7e9960a73af13c0e46220a
henning mueller <mail@nning.io>
2021-01-31 20:02:10 +0100

    Add possibility to configure rounds

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -11,7 +12,7 @@ const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
 const ALPHABET_LEN = len(ALPHABET)
 
 // Converts lower to upper case and removes non-runes but keeps space
-func Sanitize(in string) string {
+func Sanitize(in string) []rune {
 	out := []rune{}
 
 	for _, v := range in {
@@ -22,7 +23,7 @@ func Sanitize(in string) string {
 		}
 	}
 
-	return string(out)
+	return out
 }
 
 func RotateRight(a, b rune) rune {
@@ -39,49 +40,79 @@ func RotateLeft(a, b rune) rune {
 	return rune(ALPHABET[(ai-bi+ALPHABET_LEN)%ALPHABET_LEN])
 }
 
+func Encrypt(key []rune, msg []rune) []rune {
+	out := make([]rune, 0, len(msg))
+
+	ki := 0
+	alt := false
+
+	for _, v := range msg {
+		k := rune(key[ki])
+
+		c := RotateRight(v, k)
+		out = append(out, c)
+
+		if v == ' ' && alt {
+			ki = 0
+			alt = !alt
+		} else {
+			ki = (ki + 1) % len(key)
+		}
+	}
+
+	return out
+}
+
+func Decrypt(key []rune, msg []rune) []rune {
+	out := make([]rune, 0, len(msg))
+
+	ki := 0
+	alt := false
+
+	for _, v := range msg {
+		k := rune(key[ki])
+
+		c := RotateLeft(v, k)
+		out = append(out, c)
+
+		if c == ' ' && alt {
+			ki = 0
+			alt = !alt
+		} else {
+			ki = (ki + 1) % len(key)
+		}
+	}
+
+	return out
+}
+
 func main() {
-	if len(os.Args) < 4 {
-		fmt.Fprintf(os.Stderr, "%s encrypt|decrypt <key> <message>\n", os.Args[0])
+	if len(os.Args) != 4 && len(os.Args) != 5 {
+		fmt.Fprintf(os.Stderr, "%s encrypt|decrypt <key> <message> [rounds]\n", os.Args[0])
 		os.Exit(1)
 	}
 
 	key := Sanitize(os.Args[2])
 	msg := Sanitize(os.Args[3])
 
-	out := make([]rune, 0, len(msg))
+	rounds := 1
 
-	ki := 0
-	alt := false
+	if len(os.Args) == 5 {
+		rounds, _ = strconv.Atoi(os.Args[4])
+	}
+
+	out := make([]rune, len(msg))
+	copy(out, msg)
 
 	if os.Args[1] == "encrypt" {
-		for _, v := range msg {
-			k := rune(key[ki])
-
-			c := RotateRight(v, k)
-			out = append(out, c)
-
-			if v == ' ' && alt {
-				ki = 0
-				alt = !alt
-			} else {
-				ki = (ki + 1) % len(key)
-			}
+		for i := 0; i < rounds; i++ {
+			out = Encrypt(key, out)
 		}
 	}
 
 	if os.Args[1] == "decrypt" {
-		for _, v := range msg {
-			k := rune(key[ki])
-
-			c := RotateLeft(v, k)
-			out = append(out, c)
-
-			if c == ' ' && alt {
-				ki = 0
-				alt = !alt
-			} else {
-				ki = (ki + 1) % len(key)
-			}
+		for i := 0; i < rounds; i++ {
+			out = Decrypt(key, out)
 		}
 	}
 
